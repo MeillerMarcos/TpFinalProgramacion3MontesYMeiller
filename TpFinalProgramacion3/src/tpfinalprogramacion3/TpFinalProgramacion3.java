bab7bb8eb2de78f703b0f40ed89ca0d900f3c80a
lautarogit <lautarojulianljm@gmail.com>
2020-06-14 00:20:33 -0300

    3 commit

--- TpFinalProgramacion3/src/tpfinalprogramacion3/TpFinalProgramacion3.java
+++ TpFinalProgramacion3/src/tpfinalprogramacion3/TpFinalProgramacion3.java
@@ -1,11 +1,10 @@
 package tpfinalprogramacion3;
 
-
-public class TpFinalProgramacion3 {
-
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public class TpFinalProgramacion3 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("primer commit");
+        System.out.println("segundo");
+    } 
 }
